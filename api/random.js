a08c901c59f8a6f14221165e5b0b939d4189f998
Syed Adam Ali <kingsilverali@gmail.com>
2026-03-17 00:31:31 +0530

    a

--- api/random.js
+++ api/random.js
@@ -1,16 +1,29 @@
-import fs from "fs";
-import path from "path";
+import fs from "fs"
+import path from "path"
 
 export default function handler(req, res) {
 
-  const folder = path.join(process.cwd(), "avatars/all");
-  const files = fs.readdirSync(folder);
+  try {
 
-  const random = files[Math.floor(Math.random() * files.length)];
+    const folder = path.join(process.cwd(), "avatars/all")
 
-  const imagePath = path.join(folder, random);
-  const image = fs.readFileSync(imagePath);
+    const files = fs.readdirSync(folder)
 
-  res.setHeader("Content-Type", "image/png");
-  res.send(image);
+    const random = files[Math.floor(Math.random() * files.length)]
+
+    const imagePath = path.join(folder, random)
+
+    const image = fs.readFileSync(imagePath)
+
+    res.setHeader("Content-Type", "image/png")
+
+    res.status(200).send(image)
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    })
+
+  }
 }
