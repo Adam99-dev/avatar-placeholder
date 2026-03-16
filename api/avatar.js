13f2568a744dd81f015b9f7c3c599e08d64ef7a8
Syed Adam Ali <kingsilverali@gmail.com>
2026-03-17 00:48:25 +0530

    Final Commit

--- api/avatar.js
+++ api/avatar.js
@@ -2,16 +2,22 @@ export default function handler(req, res) {
 
   const { gender, id } = req.query;
 
-  let folder = "all";
+  let avatarId;
 
-  if (gender === "boy") folder = "boy";
-  if (gender === "girl") folder = "girl";
+  if (id) {
+    avatarId = id;
+  } 
+  else if (gender === "boy") {
+    avatarId = Math.floor(Math.random() * 50) + 1;      // 1-50
+  } 
+  else if (gender === "girl") {
+    avatarId = Math.floor(Math.random() * 50) + 51;     // 51-100
+  } 
+  else {
+    avatarId = Math.floor(Math.random() * 100) + 1;     // 1-100
+  }
 
-  const max = 10; // number of images in each folder
-
-  const avatarId = id || Math.floor(Math.random() * max) + 1;
-
-  const url = `/avatars/${folder}/AV${avatarId}.png`;
+  const url = `/avatars/AV${avatarId}.png`;
 
   res.writeHead(302, {
     Location: url
